6aac30f1a5469a0d7e2fa3faaba685b48e08688f
fahad-zakir <fahadzakir81@gmail.com>
2021-08-30 18:56:00 -0500

    added-dot-env-and-url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-const API_URL = ``;
+const API_URL = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${process.env.REACT_APP_API_KEY}`;
+console.log(API_URL)
 
 function App() {
   return (
